683c7008452dd96de958d9fd9b0983d5e6cf345d
Siarhei Karol <skarol@ebsco.com>
2024-11-01 19:44:02 +0500

    modify config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,27 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const path = require('path');
-const config = require('@folio/jest-config-stripes');
+
+const esModules = ['@folio', 'ky'].join('|');
 
 module.exports = {
-  ...config,
-  setupFiles: [
-    ...config.setupFiles,
-    path.join(__dirname, './test/jest/jest.setup.js'),
+  collectCoverageFrom: [
+    '**/(lib|src)/**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/test/jest/**',
   ],
+  coverageDirectory: './artifacts/coverage-jest/',
+  coverageReporters: ['lcov'],
+  reporters: ['jest-junit', 'default'],
+  transform: { '^.+\\.(js|jsx)$': path.join(__dirname, './test/jest/jest-transformer.js') },
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+  moduleNameMapper: {
+    '^.+\\.(css|png|svg)$': 'identity-obj-proxy',
+  },
+  testMatch: ['**/(lib|src)/**/?(*.)test.{js,jsx}'],
+  testPathIgnorePatterns: ['/node_modules/', '/test/bigtest/', '/test/ui-testing/'],
+  setupFiles: [
+    path.join(__dirname, './test/jest/setupTests.js'),
+    'jest-canvas-mock'
+  ],
+  setupFilesAfterEnv: [path.join(__dirname, './test/jest/jest.setup.js')],
 };
